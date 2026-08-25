04a131ef794f3e0f9d83bc0b23a0b3d986448613
brayan <brayan_anzini@estudante.sesisenai.org.br>
2026-08-25 11:37:15 -0300

    add- criei a estrutura inicial do arquivo index.php

---
+++ public_cliente/index.php
@@ -0,0 +1,27 @@
+<<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <title>Patinhas com Segurança</title>
+    </head>
+
+    <body>
+    <h1>Patinhas com Segurança</h1>
+
+    <h2>Menu</h2>
+
+    <a href= "public_cliente/cadastrar_cliente.php">
+        clientes
+    </a>
+
+    <br><br>
+
+    <a href= "public_animal/cadastrar_animal.php">
+        animais
+    </a>
+
+</body>
+
+</html>
