3bbbedf22269f73fde89dc6ca8615bfec2ae658c
brayan <brayan_anzini@estudante.sesisenai.org.br>
2026-08-26 12:14:25 -0300

    add- implementa a funcionalidade de exclusão de animais

--- public_animal/excluir_animal.php
+++ public_animal/excluir_animal.php
@@ -1,2 +1,19 @@
 <?php
-require_once 
+
+include("../infra/conexao.php");
+
+$id = $_GET["id"];
+
+$sql = "DELETE FROM animais WHERE id = $id";
+
+if ($conn->query($sql)) {
+
+    header("Location: cadastrar_animal.php");
+
+} else {
+
+    echo "Erro ao excluir animal.";
+
+}
+
+?>
